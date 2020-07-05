f98cf95fa2f6f26ed018ce3bb93931001c9e926e
Magnus Toftdal <duckhero@gmail.com>
2020-07-05 16:15:38 +0200

    Added stub controllers

---
+++ app/Http/Controllers/Order/CreateOrderController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Order;
+
+use Illuminate\Http\Request;
+
+class CreateOrderController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Order/DeleteOrderController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Order;
+
+use Illuminate\Http\Request;
+
+class DeleteOrderController extends Controller
+{
+    //
+}
